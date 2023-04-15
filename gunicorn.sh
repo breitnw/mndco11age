5cc94e994c6212be475dbd21e343f68debaccfb8
Nicholas Breitling <breitling.nw@gmail.com>
2023-04-15 16:03:44 -0500

    Switched to Rust for fun

--- gunicorn.sh
+++
@@ -1,1 +0,0 @@
-sudo gunicorn 'app:create_app()' -w 2 --certfile '/etc/letsencrypt/live/mndco11age.xyz/fullchain.pem' --keyfile '/etc/letsencrypt/live/mndco11age.xyz/privkey.pem' -b 0.0.0.0:443
--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ tmux new-session -d -s $session
 
 window=0
 tmux rename-window -t $session:$window 'run'
-tmux send-keys -t $session:$window "./gunicorn.sh" C-m
+tmux send-keys -t $session:$window "cargo run --release" C-m
 
 echo "HTTPS server running on port 443"
 
