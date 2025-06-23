ab3b54955f09aff653443541c353d42b47635627
Nick Breitling <breitling.nw@gmail.com>
2025-06-23 15:27:37 -0500

    loading animations

--- static/masonry.js
+++ static/masonry.js
@@ -1,32 +1,51 @@
 // This code is adapted from Andy Barefoot's guide. Thank you!
 // https://medium.com/@andybarefoot/a-masonry-style-layout-using-css-grid-8c663d355ebb
 
-function resizeGridItem(item){
+function resizeCard(item){
   grid = document.getElementsByClassName("masonry")[0];
-  rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-  rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-  console.log(rowGap)
-  rowSpan = Math.ceil((item.querySelector(".contents").getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+  grid_style = window.getComputedStyle(grid);
+  rowHeight = parseInt(grid_style.getPropertyValue('grid-auto-rows'));
+  rowGap = parseInt(grid_style.getPropertyValue('grid-row-gap'));
+  rowSpan = Math.ceil((item.querySelector(".card-contents").getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
     item.style.gridRowEnd = "span "+rowSpan;
 }
 
-function resizeAllGridItems(){
+function resizeAllCards(){
   allItems = document.getElementsByClassName("card");
   for(x=0;x<allItems.length;x++){
-    resizeGridItem(allItems[x]);
+    resizeCard(allItems[x]);
   }
 }
 
-function resizeInstance(instance){
+function loadInstance(instance){
 	item = instance.elements[0];
-  resizeGridItem(item);
+  item.classList.add("loaded")
+
+  // every time a new instance is loaded, resize all cards to fit with the layout
+	resizeAllCards();
 }
 
-window.addEventListener("resize", resizeAllGridItems);
-window.addEventListener("load", resizeAllGridItems);
+// resize cards when the window size changes
+window.addEventListener("resize", resizeAllCards);
 
-// also resize incrementally on load
+// resize incrementally on load
 allItems = document.getElementsByClassName("card");
 for(x=0;x<allItems.length;x++){
-  imagesLoaded(allItems[x], resizeInstance);
+  const item = allItems[x]
+  const image = item.querySelector("img")
+  const loadingAnimation = item.querySelector(".loading-animation")
+
+  imagesLoaded(allItems[x], loadInstance);
+
+  // if (image.decode) {
+  //   image.decode()
+  //     .then(() => {
+  //       console.log('GIF ready with dimensions', item.naturalWidth, item.naturalHeight);
+  //       resizeAllCards()
+  //       // resizeCard(item)
+  //     })
+  // }
 }
+
+// finally, resize now
+resizeAllCards();
