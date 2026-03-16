085e4e02c7e557e2783fd64cd33093e9cac29e51
Nick Breitling <breitling.nw@gmail.com>
2026-03-15 22:54:55 -0500

    wasted lots of time

---
+++ static/images/make-stickers.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -p stickers-out
+for file in stickers-original/*; do
+    newfile=stickers-out/$(basename $file)
+    echo "converting $file to $newfile"
+    if [ -f "$file" ]; then
+        magick $file -resize 2048@ -background none -bordercolor none \
+            -gravity center -border 20 -write mpr:in -resize 200% \
+            -channel A -morphology dilate disk:10 +channel -fill white \
+            -colorize 100 -resize 50% mpr:in -composite $newfile
+    fi
+done
