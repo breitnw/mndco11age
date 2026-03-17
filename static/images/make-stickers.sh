82d9d0a4aea7b3ccf39b512b632d07fb077d5582
Nick Breitling <breitling.nw@gmail.com>
2026-03-17 12:17:21 -0500

    new stickers

--- static/images/make-stickers.sh
+++ static/images/make-stickers.sh
@@ -4,10 +4,11 @@ mkdir -p stickers-out
 for file in stickers-original/*; do
     newfile=stickers-out/$(basename $file)
     echo "converting $file to $newfile"
-    if [ -f "$file" ]; then
+    if [ -f "$file" ] && [ ! -f "$newfile" ]; then
         magick $file -resize 2048@ -background none -bordercolor none \
             -gravity center -border 20 -write mpr:in -resize 200% \
             -channel A -morphology dilate disk:10 +channel -fill white \
-            -colorize 100 -resize 50% mpr:in -composite $newfile
+            -colorize 100 -resize 50% mpr:in -composite \
+	    -channel A -evaluate multiply 5 $newfile
     fi
 done
